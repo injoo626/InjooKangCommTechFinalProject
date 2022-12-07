abc983cdcfca5c4250f1889ffcf673ecdec936bc
injoo626 <57341200+injoo626@users.noreply.github.com>
2022-12-07 10:15:18 +0400

    Add files via upload

---
+++ java/indexJava.js
@@ -0,0 +1,20 @@
+var bgImageArray = ["bg1.jpeg", "bg2.jpeg", "bg3.jpeg","bg4.jpeg","bg5.jpeg","bg6.jpeg"],
+base = "images/",
+secs = 4;
+bgImageArray.forEach(function(img){
+    new Image().src = base + img; 
+    // caches images, avoiding white flash between background replacements
+});
+
+function backgroundSequence() {
+	window.clearTimeout();
+	var k = 0;
+	for (i = 0; i < bgImageArray.length; i++) {
+		setTimeout(function(){ 
+			document.documentElement.style.background = "url(" + base + bgImageArray[k] + ") no-repeat center center fixed";
+			document.documentElement.style.backgroundSize ="cover";
+		if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }			
+		}, (secs * 1000) * i)	
+	}
+}
+backgroundSequence();
